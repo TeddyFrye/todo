31cc1ce17219bdcd3d83e37657fe4b7546d15ca1
TeddyFrye <tfrye1227@gmail.com>
2023-07-28 15:05:19 -0500

    Fixes bugs with disappearing bugs, adds edit ability

--- storage.js
+++ storage.js
@@ -1,13 +1,21 @@
 // Store a project
 function storeProject(project) {
-  localStorage.setItem(project.getTitle(), JSON.stringify(project));
+  if (Array.isArray(project)) {
+    console.error("Attempted to store an array as a project:", project);
+  } else {
+    const key = `project-${project.getTitle()}`; // Added project- prefix
+    console.log("Storing project:", project);
+    localStorage.setItem(key, JSON.stringify(project));
+  }
 }
 
 // Retrieve a project
-function getProject(title) {
-  let project = localStorage.getItem(title);
+function getProject(key) {
+  // Changed parameter name to key for clarity
+  let project = localStorage.getItem(key);
   if (project) {
     project = JSON.parse(project);
+    console.log("Retrieved project:", project);
 
     // Since localStorage won't keep the methods of the stored objects, you'll need to recreate them as Projects
     let newProject = new Project(project.title);
@@ -31,7 +39,8 @@ function getProject(title) {
 
 // Delete a project
 function deleteProject(title) {
-  localStorage.removeItem(title);
+  const key = `project-${title}`; // Added project- prefix
+  localStorage.removeItem(key);
 }
 
 // Store all projects
@@ -45,13 +54,16 @@ function storeAllProjects(projects) {
 function getAllProjects() {
   let projects = [];
 
-  for (let i = 0; i < localStorage.length; i++) {
-    let title = localStorage.key(i);
-    let project = getProject(title);
+  const keys = Object.keys(localStorage).filter((key) =>
+    key.startsWith("project-")
+  ); // Filter keys that start with project-
+
+  keys.forEach((key) => {
+    let project = getProject(key);
     if (project) {
       projects.push(project);
     }
-  }
+  });
 
   return projects;
 }
@@ -59,5 +71,6 @@ function getAllProjects() {
 //Delete project from storage
 window.deleteProjectFromStorage = function (title) {
   // Use the localStorage.removeItem method to delete the project from storage
-  localStorage.removeItem(title);
+  const key = `project-${title}`; // Added project- prefix
+  localStorage.removeItem(key);
 };
--- ui.js
+++ ui.js
@@ -1,3 +1,8 @@
+// Check if 'projects' is not already set in localStorage
+if (!localStorage.getItem("projects")) {
+  // Set 'projects' to an empty array
+  localStorage.setItem("projects", JSON.stringify([]));
+}
 // This function will be used to render a single todo
 function renderTodo(todo) {
   // Create a new div to contain the todo
@@ -9,10 +14,23 @@ function renderTodo(todo) {
 
   // Set the content of the title and dueDate element
   titleElement.textContent = todo.getTitle();
+  titleElement.contentEditable = "true";
   titleElement.className = `todo-title todo-priority-${todo
     .getPriority()
     .toLowerCase()}`;
   dueDateElement.textContent = todo.getDueDate();
+
+  // Add an event listener for 'keypress'
+  titleElement.addEventListener("keypress", function (e) {
+    // Check if the key pressed was 'Enter'
+    if (e.key === "Enter") {
+      e.preventDefault(); // Prevent the default action
+      todo.setTitle(e.target.textContent); // Update the todo title
+      let projectTitle =
+        e.target.parentElement.parentElement.querySelector("h2").textContent;
+      updateTodoInStorage(todo, projectTitle); // Update the todo in the storage
+    }
+  });
 
   // Append the title and dueDate element to the todoDiv
   todoDiv.appendChild(titleElement);
@@ -20,6 +38,40 @@ function renderTodo(todo) {
 
   // Append the todoDiv to the todo container in your HTML
   document.querySelector("#todo-container").appendChild(todoDiv);
+}
+function updateProjectInStorage(updatedProject) {
+  // Get all projects from storage
+  let projects = JSON.parse(localStorage.getItem("projects"));
+
+  // Find the index of the project to update
+  let index = projects.findIndex(
+    (project) => project.title === updatedProject.getTitle()
+  );
+
+  // Replace the old project with the updated one
+  projects[index] = updatedProject;
+
+  // Store the updated projects array back in storage
+  localStorage.setItem("projects", JSON.stringify(projects));
+}
+
+function updateTodoInStorage(updatedTodo, projectTitle) {
+  // Get all projects from storage
+  let projects = JSON.parse(localStorage.getItem("projects"));
+
+  // Find the index of the project this todo belongs to
+  let projectIndex = projects.findIndex((proj) => proj.title === projectTitle);
+
+  // Find the index of the todo to update in the project's todos array
+  let todoIndex = projects[projectIndex].todos.findIndex(
+    (todo) => todo.title === updatedTodo.getTitle()
+  );
+
+  // Replace the old todo with the updated one
+  projects[projectIndex].todos[todoIndex] = updatedTodo;
+
+  // Store the updated projects array back in storage
+  localStorage.setItem("projects", JSON.stringify(projects));
 }
 
 window.renderProject = function (project) {
@@ -44,6 +96,18 @@ window.renderProject = function (project) {
   // Create and append title element to projectDiv
   let titleElement = document.createElement("h2");
   titleElement.textContent = project.getTitle();
+  titleElement.contentEditable = "true";
+
+  // Add an event listener for 'keypress'
+  titleElement.addEventListener("keypress", function (e) {
+    // Check if the key pressed was 'Enter'
+    if (e.key === "Enter") {
+      e.preventDefault(); // Prevent the default action
+      project.setTitle(e.target.textContent); // Update the project title
+      updateProjectInStorage(project); // Update the project in the storage
+    }
+  });
+
   projectDiv.appendChild(titleElement);
 
   // Render each todo in the project
